e03781ebbccf10b8c8b11bb4ccfb360d0deefe42
mldxo <miloszmaculewicz@gmail.com>
2024-07-07 16:20:10 +0200

    Comment out Nuklear template

--- client/src/client.c
+++ client/src/client.c
@@ -41,41 +41,40 @@ void *receive_messages(void *socket_desc)
 
 void run_client()
 {
-    // Initialize Nuklear - template from https://immediate-mode-ui.github.io/Nuklear/doc/index.html#nuklear/example
-    enum {EASY, HARD};
-    static int op = EASY;
-    static float value = 0.6f;
+    // // Initialize Nuklear - template from https://immediate-mode-ui.github.io/Nuklear/doc/index.html#nuklear/example
+    // enum {EASY, HARD};
+    // static int op = EASY;
+    // static float value = 0.6f;
 
-    struct nk_context ctx;
-    nk_init_fixed(&ctx, calloc(1, MAX_MEMORY), MAX_MEMORY, NULL);
-    if (nk_begin(&ctx, "Show", nk_rect(50, 50, 220, 220), NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_CLOSABLE))
-        {
+    // struct nk_context ctx;
+    // nk_init_fixed(&ctx, calloc(1, MAX_MEMORY), MAX_MEMORY, NULL);
+    // if (nk_begin(&ctx, "Show", nk_rect(50, 50, 220, 220), NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_CLOSABLE))
+    //     {
 
-        nk_layout_row_static(&ctx, 30, 80, 1);
-        if (nk_button_label(&ctx, "button"))
-        {
+    //     nk_layout_row_static(&ctx, 30, 80, 1);
+    //     if (nk_button_label(&ctx, "button"))
+    //     {
 
-        }
+    //     }
 
-        nk_layout_row_dynamic(&ctx, 30, 2);
-        if (nk_option_label(&ctx, "easy", op == EASY))
-            op = EASY;
-        if (nk_option_label(&ctx, "hard", op == HARD))
-            op = HARD;
+    //     nk_layout_row_dynamic(&ctx, 30, 2);
+    //     if (nk_option_label(&ctx, "easy", op == EASY))
+    //         op = EASY;
+    //     if (nk_option_label(&ctx, "hard", op == HARD))
+    //         op = HARD;
 
-        nk_layout_row_begin(&ctx, NK_STATIC, 30, 2);
-        {
-            nk_layout_row_push(&ctx, 50);
-            nk_label(&ctx, "Volume:", NK_TEXT_LEFT);
-            nk_layout_row_push(&ctx, 110);
-            nk_slider_float(&ctx, 0, &value, 1.0f, 0.1f);
-        }
-        nk_layout_row_end(&ctx);
-    }
-    nk_end(&ctx);
+    //     nk_layout_row_begin(&ctx, NK_STATIC, 30, 2);
+    //     {
+    //         nk_layout_row_push(&ctx, 50);
+    //         nk_label(&ctx, "Volume:", NK_TEXT_LEFT);
+    //         nk_layout_row_push(&ctx, 110);
+    //         nk_slider_float(&ctx, 0, &value, 1.0f, 0.1f);
+    //     }
+    //     nk_layout_row_end(&ctx);
+    // }
+    // nk_end(&ctx);
 
-    ////
-
+    // client
     int sock;
     struct sockaddr_in server_addr;
     char message[BUFFER_SIZE];
