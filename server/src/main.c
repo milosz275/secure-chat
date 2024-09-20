997747fc4b79b1da2b2584f9ab6ab25fbb7d1a91
mldxo <miloszmaculewicz@gmail.com>
2024-09-20 14:04:35 +0200

    Disable standard output buffering, use exit codes

--- server/src/main.c
+++ server/src/main.c
@@ -1,6 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "server.h"
 
 int main()
 {
-    run_server();
+    setbuf(stdout, NULL);
+
+    int result = run_server();
+    if (result != 0)
+    {
+        fprintf(stderr, "Server failed to run with error code: %d\n", result);
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
