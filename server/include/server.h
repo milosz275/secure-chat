c27a0f04bbf065d5a7800b70aa81638749bb6feb
mldxo <miloszmaculewicz@gmail.com>
2024-09-22 12:43:50 +0200

    Add database connection error codes

--- server/include/server.h
+++ server/include/server.h
@@ -7,6 +7,14 @@
 #define MAX_CLIENTS 100
 
 /**
+ * The database connection exit codes.
+ */
+#define DATABASE_CONNECTION_SUCCESS 200
+#define DATABASE_OPEN_FAILURE 201
+#define DATABASE_CREATE_USERS_TABLE_FAILURE 202
+#define DATABASE_CREATE_MESSAGES_TABLE_FAILURE 203
+
+/**
  * The singular request structure. This structure is used to store server connection data.
  * 
  * @param address The request address.
@@ -45,11 +53,20 @@ struct clients_t
 };
 
 /**
- * Client handler. This function is used to handle a client connected to the server and is supposed to run in a separate thread.
+ * Database connector. This function is used to connect to a SQLite3 database and create it with its tables if it does not exist.
  * 
+ * @param db The SQLite3 database.
+ * @param db_name The name of the database.
+ * @return The database connection exit code.
+ */
+int connect_db(sqlite3 **db, char *db_name);
+
+/**
+ * Client handler. This function is used to handle a client connected to the server and is supposed to run in a separate thread.
+ *
  * @param arg The client structure.
  */
-void* handle_client(void* arg);
+void *handle_client(void *arg);
 
 /**
  * Runs the server. This function is meant to be called by the main function of the server program.
