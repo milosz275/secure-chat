ecd72902dc1498b8d8cc2f43e252640e59b76552
mldxo <miloszmaculewicz@gmail.com>
2024-09-23 15:25:40 +0200

    Add create, parse, send message, message to string functions

--- common/src/protocol.c
+++ common/src/protocol.c
@@ -8,7 +8,68 @@
 #include <string.h>
 #include <time.h>
 #include <signal.h>
+#include <arpa/inet.h>
 #include <openssl/evp.h>
+
+int create_message(message_t* msg, message_type_t type, const char* sender_uid, const char* recipient_uid, const char* payload)
+{
+    if (msg == NULL || sender_uid == NULL || recipient_uid == NULL || payload == NULL)
+    {
+        return MESSAGE_CREATION_FAILURE;
+    }
+
+    snprintf(msg->message_uid, MESSAGE_HASH_LENGTH, "%s", generate_uid(payload, MESSAGE_HASH_LENGTH));
+    msg->type = type;
+    snprintf(msg->sender_uid, USERNAME_HASH_LENGTH, "%s", sender_uid);
+    snprintf(msg->recipient_uid, USERNAME_HASH_LENGTH, "%s", recipient_uid);
+    msg->payload_length = strlen(payload);
+    snprintf(msg->payload, MAX_PAYLOAD_SIZE, "%s", payload);
+
+    return MESSAGE_CREATION_SUCCESS;
+}
+
+void parse_message(message_t* msg, const char* buffer)
+{
+    sscanf(buffer, "%63[^|]|%d|%63[^|]|%63[^|]|%u|%1023[^\n]",
+        msg->message_uid, (int*)&msg->type, msg->sender_uid, msg->recipient_uid, &msg->payload_length, msg->payload);
+}
+
+int send_message(int socket, message_t* msg)
+{
+    if (msg == NULL)
+    {
+        return MESSAGE_SEND_FAILURE;
+    }
+
+    char buffer[BUFFER_SIZE];
+    snprintf(buffer, BUFFER_SIZE, "%s%s%d%s%s%s%s%s%u%s%s",
+        msg->message_uid, MESSAGE_DELIMITER,
+        msg->type, MESSAGE_DELIMITER,
+        msg->sender_uid, MESSAGE_DELIMITER,
+        msg->recipient_uid, MESSAGE_DELIMITER,
+        msg->payload_length, MESSAGE_DELIMITER,
+        msg->payload);
+    send(socket, buffer, strlen(buffer), 0);
+    return MESSAGE_SEND_SUCCESS;
+}
+
+const char* message_type_to_string(message_type_t type)
+{
+    switch (type)
+    {
+    case MESSAGE_TEXT: return "MESSAGE_TEXT";
+    case MESSAGE_PING: return "MESSAGE_PING";
+    case MESSAGE_SIGNAL: return "MESSAGE_SIGNAL";
+    case MESSAGE_AUTH: return "MESSAGE_AUTH";
+    case MESSAGE_ACK: return "MESSAGE_ACK";
+    case MESSAGE_ERROR: return "MESSAGE_ERROR";
+    case MESSAGE_USER_JOIN: return "MESSAGE_USER_JOIN";
+    case MESSAGE_USER_LEAVE: return "MESSAGE_USER_LEAVE";
+    case MESSAGE_GROUP: return "MESSAGE_GROUP";
+    case MESSAGE_SYSTEM: return "MESSAGE_SYSTEM";
+    default: return "UNKNOWN";
+    }
+}
 
 const unsigned char* get_hash(const char* password)
 {
@@ -47,7 +108,7 @@ const char* generate_uid(const char* text, int hash_length)
 
 const char* generate_unique_user_id(const char* username)
 {
-    return generate_uid(username, CLIENT_HASH_LENGTH);
+    return generate_uid(username, USERNAME_HASH_LENGTH);
 }
 
 void int_handler(int sig)
