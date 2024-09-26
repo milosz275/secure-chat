455db85816a6e196e93e42f828271f1964bee1fc
mldxo <miloszmaculewicz@gmail.com>
2024-09-26 19:39:04 +0200

    Add timestamp, fix bugs

--- common/src/log.c
+++ common/src/log.c
@@ -7,40 +7,76 @@
 #include <errno.h>
 #include <pthread.h>
 #include <sys/file.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include "protocol.h"
 
 static struct loggers_t loggers = { PTHREAD_MUTEX_INITIALIZER, {NULL} };
+
+int fileno(FILE* __stream);
 
 void usleep(unsigned int usec);
 
 void init_logging(const char* filename)
 {
+    const char* log_dir = LOGS_DIR;
+    struct stat st = { 0 };
+
+    if (stat(log_dir, &st) == -1)
+    {
+        if (mkdir(log_dir, 0700) != 0)
+        {
+            perror("Failed to create logs directory");
+            return;
+        }
+    }
+
+    char full_path[256];
+    snprintf(full_path, sizeof(full_path), "%s/%s", log_dir, filename);
+
     pthread_mutex_lock(&loggers.log_mutex);
     for (int i = 0; i < MAX_LOG_FILES; i++)
     {
-        if (loggers.array[i] == NULL) {
+        if (loggers.array[i] == NULL)
+        {
             loggers.array[i] = (struct logger_t*)malloc(sizeof(logger_t));
-            loggers.array[i]->filename = filename;
-            loggers.array[i]->file = fopen(filename, "a"); 
+            loggers.array[i]->filename = (char*)malloc(strlen(full_path) + 1);
+            if (loggers.array[i]->filename == NULL)
+            {
+                perror("Failed to allocate memory for filename");
+                free(loggers.array[i]);
+                loggers.array[i] = NULL;
+                break;
+            }
+            snprintf(loggers.array[i]->filename, strlen(full_path) + 1, "%s", full_path);
+            loggers.array[i]->file = fopen(full_path, "a");
             if (loggers.array[i]->file == NULL)
             {
                 perror("Failed to open log file");
+                free(loggers.array[i]->filename);
                 free(loggers.array[i]);
                 loggers.array[i] = NULL;
             }
             break;
         }
     }
-    pthread_mutex_unlock(&loggers.log_mutex); 
+    pthread_mutex_unlock(&loggers.log_mutex);
 }
 
-void log_message(log_level_t level, const char* filename, const char* message)
+void log_message(log_level_t level, const char* filename, const char* source_file, const char* message)
 {
+    const char* log_dir = LOGS_DIR;
+
+    char full_path[256];
+    snprintf(full_path, sizeof(full_path), "%s/%s", log_dir, filename);
+
     pthread_mutex_lock(&loggers.log_mutex);
 
     FILE* log = NULL;
     for (int i = 0; i < MAX_LOG_FILES; i++)
     {
-        if (loggers.array[i] != NULL && strcmp(loggers.array[i]->filename, filename) == 0)
+        if (loggers.array[i] != NULL && strcmp(loggers.array[i]->filename, full_path) == 0)
         {
             log = loggers.array[i]->file;
             break;
@@ -50,27 +86,26 @@ void log_message(log_level_t level, const char* filename, const char* message)
     if (log == NULL)
     {
         pthread_mutex_unlock(&loggers.log_mutex);
-        fprintf(stderr, "Log file not found: %s\n", filename);
+        fprintf(stderr, "Log file not found: %s\n", full_path);
         return;
     }
 
     int timeout = 0;
     int error = 0;
 
-    rewind(log);
-    error = lockf(fileno(log), F_TLOCK, 0);
-    while (error == EACCES || error == EAGAIN)
+    error = flock(fileno(log), LOCK_EX | LOCK_NB);
+    while (error == -1 && (errno == EWOULDBLOCK || errno == EAGAIN))
     {
         usleep(LOCKED_FILE_RETRY_TIME);
         timeout += LOCKED_FILE_RETRY_TIME;
 
         if (timeout > LOCKED_FILE_TIMEOUT)
         {
-            fprintf(stderr, "Log file locking timed out: %s\n", filename);
+            fprintf(stderr, "Log file locking timed out: %s\n", full_path);
             pthread_mutex_unlock(&loggers.log_mutex);
             return;
         }
-        error = lockf(fileno(log), F_TLOCK, 0);
+        error = flock(fileno(log), LOCK_EX | LOCK_NB);
     }
 
     fseek(log, 0, SEEK_END);
@@ -86,14 +121,14 @@ void log_message(log_level_t level, const char* filename, const char* message)
     default: level_str = "UNKNOWN"; break;
     }
 
-    fprintf(log, "[%s] %s\n", level_str, message);
+    const char* timestamp = get_formatted_timestamp();
+    fprintf(log, "%s - %s - %s - %s\n", timestamp, level_str, source_file, message);
 
     fflush(log);
 
-    rewind(log);
-    lockf(fileno(log), F_ULOCK, 0);
+    flock(fileno(log), LOCK_UN);
 
-    pthread_mutex_unlock(&loggers.log_mutex); 
+    pthread_mutex_unlock(&loggers.log_mutex);
 }
 
 void finish_logging()
@@ -108,5 +143,5 @@ void finish_logging()
             loggers.array[i] = NULL;
         }
     }
-    pthread_mutex_unlock(&loggers.log_mutex); 
+    pthread_mutex_unlock(&loggers.log_mutex);
 }
