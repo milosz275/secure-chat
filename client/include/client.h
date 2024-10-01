c203df107ddb065d6648cd1d1f7214fa16401352
mldxo <miloszmaculewicz@gmail.com>
2024-10-01 22:45:09 +0200

    Update server reconnection defines

--- client/include/client.h
+++ client/include/client.h
@@ -3,8 +3,8 @@
 
 #include "nuklear.h"
 
-#define SERVER_RECONNECTION_INTERVAL 3
-#define SERVER_RECONNECTION_ATTEMPTS 60
+#define SERVER_RECONNECTION_INTERVAL 1
+#define SERVER_RECONNECTION_ATTEMPTS 120
 
 /**
  * Receive messages from the server. This function is meant to be run in a separate thread and handle the reception of messages from the server.
