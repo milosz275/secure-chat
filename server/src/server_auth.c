ecbc2b9039b09d368f62fe626dd16deaf9ad1ff7
mldxo <miloszmaculewicz@gmail.com>
2024-10-02 21:33:37 +0200

    Log request username

--- server/src/server_auth.c
+++ server/src/server_auth.c
@@ -73,6 +73,10 @@ int user_auth(request_t* req, client_connection_t* cl)
         snprintf(username, MAX_USERNAME_LENGTH, "%.*s", MAX_USERNAME_LENGTH - 1, msg.payload);
         username[MAX_USERNAME_LENGTH] = '\0';
 
+        log_msg[0] = '\0';
+        sprintf(log_msg, "Request from %s:%d for username %s", inet_ntoa(req->address.sin_addr), ntohs(req->address.sin_port), username);
+        log_message(LOG_INFO, REQUESTS_LOG, __FILE__, log_msg);
+
         char* sql = "SELECT uid FROM users WHERE username = ?;";
 
         if (sqlite3_prepare_v2(db, sql, -1, &stmt, 0) != SQLITE_OK)
@@ -238,7 +242,9 @@ int user_auth(request_t* req, client_connection_t* cl)
                 else
                 {
                     log_msg[0] = '\0';
-                    sprintf(log_msg, "Request from %s:%d failed authentication - invalid choice", inet_ntoa(req->address.sin_addr), ntohs(req->address.sin_port));
+                    char choice_truncated[4];
+                    snprintf(choice_truncated, 4, "%.*s", 3, msg.payload);
+                    sprintf(log_msg, "Request from %s:%d failed authentication - invalid choice: %s", inet_ntoa(req->address.sin_addr), ntohs(req->address.sin_port), choice_truncated);
                     log_message(LOG_INFO, REQUESTS_LOG, __FILE__, log_msg);
                     goto cleanup;
                 }
