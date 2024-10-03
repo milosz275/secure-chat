3a7973e1711adedcfd8d4d8de5b87641f01abba0
mldxo <miloszmaculewicz@gmail.com>
2024-10-03 15:28:19 +0200

    Add client state

--- client/include/client.h
+++ client/include/client.h
@@ -12,15 +12,27 @@
  * @param socket The client socket.
  * @param uid The client unique ID.
  * @param username The client username.
- * @param is_authenticated The client authentication status.
  */
 typedef struct client_t
 {
     int socket;
     char* uid;
     char username[MAX_USERNAME_LENGTH + 1];
-    int is_authenticated;
 } client_t;
+
+/**
+ * The client state structure. This structure is used to define the client state structure.
+ *
+ * @param is_entering_username The client is entering the username.
+ * @param is_entering_password The client is entering the password.
+ * @param is_authenticated The client is authenticated.
+ */
+typedef struct client_state_t
+{
+    int is_entering_username;
+    int is_entering_password;
+    int is_authenticated;
+} client_state_t;
 
 /**
  * Receive messages from the server. This function is meant to be run in a separate thread and handle the reception of messages from the server.
