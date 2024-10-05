7183cd80bf3ab447efed7622b050718592a9d6c1
mldxo <miloszmaculewicz@gmail.com>
2024-10-05 12:35:12 +0200

    Update command list

--- server/src/server_cli.c
+++ server/src/server_cli.c
@@ -10,10 +10,13 @@ static srv_command_t srv_commands[] =
     {.srv_command = &srv_exit, .srv_command_name = "!quit", .srv_command_description = "Exit command alias." },
     {.srv_command = &srv_exit, .srv_command_name = "!stop", .srv_command_description = "Exit command alias." },
     {.srv_command = &srv_exit, .srv_command_name = "!shutdown", .srv_command_description = "Exit command alias." },
-    {.srv_command = &srv_clear, .srv_command_name = "!clear", .srv_command_description = "Clear CLI screen." },
-    {.srv_command = &srv_ban, .srv_command_name = "!ban", .srv_command_description = "Bans given user (arg: user ID or UID)." },
-    {.srv_command = &srv_kick, .srv_command_name = "!kick", .srv_command_description = "Kicks given user (arg: user ID or UID)." },
-    {.srv_command = &srv_mute, .srv_command_name = "!mute", .srv_command_description = "Mutes given user (arg: user ID or UID)." },
+    {.srv_command = &srv_clear, .srv_command_name = "!clear", .srv_command_description = "Clears CLI screen." },
+    {.srv_command = &srv_list, .srv_command_name = "!list", .srv_command_description = "Lists authenticated users." },
+    {.srv_command = &srv_ban, .srv_command_name = "!ban", .srv_command_description = "Bans given user (arg: username, ID or UID)." },
+    {.srv_command = &srv_mute, .srv_command_name = "!mute", .srv_command_description = "Mutes given user (arg: username, ID or UID)." },
+    {.srv_command = &srv_kick, .srv_command_name = "!kick", .srv_command_description = "Kicks given user (arg: username, ID or UID)." },
+    {.srv_command = &srv_kick_all, .srv_command_name = "!kickall", .srv_command_description = "Kicks all authenticated users." },
+    {.srv_command = &srv_broadcast, .srv_command_name = "!broadcast", .srv_command_description = "Broadcast given message to all authenticated users." },
     {.srv_command = &srv_help, .srv_command_name = "!help", .srv_command_description = "Prints command descriptions." },
 };
 
@@ -21,7 +24,7 @@ int srv_help(char** args)
 {
     for (int i = 0; i < SRV_COMMANDS_NUM; ++i)
     {
-        printf("%-10s- %s\n", srv_commands[i].srv_command_name, srv_commands[i].srv_command_description);
+        printf("%-11s- %s\n", srv_commands[i].srv_command_name, srv_commands[i].srv_command_description);
     }
     if (args[0] != NULL) {}
     return 1;
