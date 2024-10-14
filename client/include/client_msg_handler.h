0223b75ff4be0b8ecc1ede181db6d6ddb803a5bd
milosz275 <miloszmaculewicz@gmail.com>
2024-10-14 22:16:19 +0200

    Add client message handler

---
+++ client/include/client_msg_handler.h
@@ -0,0 +1,32 @@
+#ifndef __CLIENT_MSG_HANDLER_H
+#define __CLIENT_MSG_HANDLER_H
+
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <unistd.h>
+#include <signal.h>
+#include <raylib.h>
+
+#include "protocol.h"
+#include "client.h"
+#include "client_gui.h"
+
+#define SERVER_RECONNECTION_INTERVAL 1
+#define SERVER_RECONNECTION_ATTEMPTS 120
+
+#define MAX_INPUT_LENGTH 128
+#define MAX_MESSAGE_LENGTH 147
+
+/**
+ * Handle a message. This function is used to handle a message received from the server.
+ *
+ * @param msg The message to handle.
+ * @param client The client structure.
+ * @param state The client state structure.
+ * @param reconnect_flag The reconnect flag.
+ * @param quit_flag The quit flag.
+ * @param server_answer The server answer flag.
+ */
+void handle_message(message_t* msg, client_t* client, client_state_t* state, volatile sig_atomic_t* reconnect_flag, volatile sig_atomic_t* quit_flag, volatile sig_atomic_t* server_answer);
+
+#endif
---
+++ client/src/client_msg_handler.c
@@ -0,0 +1,178 @@
+#include "client_msg_handler.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <raylib.h>
+
+#include "protocol.h"
+#include "client.h"
+#include "client_gui.h"
+#include "log.h"
+
+void handle_message(message_t* msg, client_t* client, client_state_t* state, volatile sig_atomic_t* reconnect_flag, volatile sig_atomic_t* quit_flag, volatile sig_atomic_t* server_answer)
+{
+    // handle PING and ACK messages
+    if (msg->type == MESSAGE_PING)
+    {
+        create_message(msg, MESSAGE_ACK, client->uid, "server", "ACK"); // [ ] handle ACK from other users
+
+        if (send_message(client->ssl, msg) != MESSAGE_SEND_SUCCESS)
+        {
+            perror("Send failed");
+            *reconnect_flag = 1;
+            pthread_exit(NULL);
+        }
+        log_message(T_LOG_INFO, CLIENT_LOG, __FILE__, "Received PING from server and sent ACK");
+        return;
+    }
+    else if (msg->type == MESSAGE_ACK)
+    {
+        if (!strcmp(msg->sender_uid, "server"))
+        {
+            log_message(T_LOG_INFO, CLIENT_LOG, __FILE__, "Received ACK from server");
+            *server_answer = 1;
+        }
+        return;
+    }
+
+    // dropping messages addressed to other users
+    if (!state->is_authenticated && strcmp(client->uid, CLIENT_DEFAULT_NAME) && msg->type != MESSAGE_PING && msg->type != MESSAGE_ACK)
+    {
+        // unauthenticated user should be addressed as CLIENT_DEFAULT_NAME
+        printf("(11111) Server: %s\n", msg->payload);
+        return;
+    }
+    else if (state->is_authenticated && strncmp((char*)msg->recipient_uid, client->uid, HASH_HEX_OUTPUT_LENGTH - 1) && msg->type != MESSAGE_UID && msg->type != MESSAGE_PING && msg->type != MESSAGE_ACK)
+    {
+        // authenticated user should be addressed by their UID
+        printf("(11112) Server: %s\n", msg->payload);
+        return;
+    }
+
+    if (msg->type == MESSAGE_AUTH && !strcmp(msg->payload, message_code_to_string(MESSAGE_CODE_ENTER_USERNAME)))
+    {
+        state->just_joined = 0;
+        state->is_entering_username = 1;
+        int code = atoi(msg->payload);
+        const char* text = message_code_to_text(code);
+        printf("(0%s) Server: %s\n", msg->payload, text);
+    }
+    else if (msg->type == MESSAGE_AUTH && !strcmp(msg->payload, message_code_to_string(MESSAGE_CODE_ENTER_PASSWORD)))
+    {
+        state->is_entering_username = 0;
+        state->is_entering_password = 1;
+        int code = atoi(msg->payload);
+        const char* text = message_code_to_text(code);
+        printf("(0%s) Server: %s\n", msg->payload, text);
+    }
+    else if (msg->type == MESSAGE_AUTH && !strcmp(msg->payload, message_code_to_string(MESSAGE_CODE_ENTER_PASSWORD_CONFIRMATION)))
+    {
+        state->is_confirming_password = 1;
+        int code = atoi(msg->payload);
+        const char* text = message_code_to_text(code);
+        printf("(0%s) Server: %s\n", msg->payload, text);
+    }
+    else if (msg->type == MESSAGE_CHOICE && !strcmp(msg->payload, message_code_to_string(MESSAGE_CODE_USER_REGISTER_CHOICE)))
+    {
+        state->is_entering_username = 0;
+        state->is_choosing_register = 1;
+        int code = atoi(msg->payload);
+        const char* text = message_code_to_text(code);
+        printf("(0%s) Server: %s\n", msg->payload, text);
+    }
+    else if (msg->type == MESSAGE_AUTH && !strcmp(msg->payload, message_code_to_string(MESSAGE_CODE_USER_ALREADY_ONLINE)))
+    {
+        reset_state(state);
+        int code = atoi(msg->payload);
+        const char* text = message_code_to_text(code);
+        printf("(0%s) Server: %s\n", msg->payload, text);
+    }
+    else if (msg->type == MESSAGE_AUTH && !strcmp(msg->payload, message_code_to_string(MESSAGE_CODE_USER_AUTHENTICATED)))
+    {
+        state->is_entering_password = 0;
+        state->is_authenticated = 1;
+    }
+    else if (msg->type == MESSAGE_AUTH && !strcmp(msg->payload, message_code_to_string(MESSAGE_CODE_USER_CREATED)))
+    {
+        state->is_confirming_password = 0;
+        state->is_authenticated = 1;
+    }
+    else if (msg->type == MESSAGE_UID)
+    {
+        if (strlen(msg->payload) != HASH_HEX_OUTPUT_LENGTH - 1)
+        {
+            printf("Invalid UID received from server\n");
+            return;
+        }
+        printf("(0%d) Server: %s%s\n", MESSAGE_CODE_UID, message_code_to_text(MESSAGE_CODE_UID), msg->payload);
+        if (client->uid != NULL)
+            strcpy(client->uid, msg->payload);
+        else
+        {
+            client->uid = malloc(strlen(msg->payload) + 1);
+            strcpy(client->uid, msg->payload);
+        }
+    }
+    else if (msg->type == MESSAGE_SIGNAL)
+    {
+        if ((!strcmp(msg->payload, MESSAGE_SIGNAL_QUIT)) || (!strcmp(msg->payload, MESSAGE_SIGNAL_EXIT)))
+        {
+            if (strcmp(msg->sender_uid, "server"))
+                log_message(T_LOG_WARN, CLIENT_LOG, __FILE__, "Received quit signal that is not from server");
+            else
+            {
+                log_message(T_LOG_INFO, CLIENT_LOG, __FILE__, "Received quit signal from server");
+                *reconnect_flag = 0;
+                *quit_flag = 1;
+                exit(EXIT_SUCCESS);
+            }
+        }
+    }
+    else if (msg->type == MESSAGE_AUTH_ATTEMPS)
+    {
+        switch (atoi(msg->payload))
+        {
+        case 3:
+            printf("(0%d) Server: %s\n", MESSAGE_CODE_USER_AUTHENTICATION_ATTEMPTS_THREE, message_code_to_text(MESSAGE_CODE_USER_AUTHENTICATION_ATTEMPTS_THREE));
+            break;
+        case 2:
+            printf("(0%d) Server: %s\n", MESSAGE_CODE_USER_AUTHENTICATION_ATTEMPTS_TWO, message_code_to_text(MESSAGE_CODE_USER_AUTHENTICATION_ATTEMPTS_TWO));
+            break;
+        case 1:
+            printf("(0%d) Server: %s\n", MESSAGE_CODE_USER_AUTHENTICATION_ATTEMPTS_ONE, message_code_to_text(MESSAGE_CODE_USER_AUTHENTICATION_ATTEMPTS_ONE));
+            break;
+        default:
+            printf("(00001) Server: %s\n", msg->payload); // unknown code
+            break;
+        }
+    }
+    else if (msg->type == MESSAGE_USER_JOIN)
+    {
+        printf("(0%d) Server: %s%s\n", MESSAGE_CODE_USER_JOIN, msg->payload, message_code_to_text(MESSAGE_CODE_USER_JOIN));
+    }
+    else if (msg->type == MESSAGE_USER_LEAVE)
+    {
+        printf("(0%d) Server: %s%s\n", MESSAGE_CODE_USER_LEAVE, msg->payload, message_code_to_text(MESSAGE_CODE_USER_LEAVE));
+    }
+    else
+    {
+        int code = atoi(msg->payload);
+        if (!code)
+        {
+            printf("(00000) Server: %s\n", msg->payload); // unknown code
+            char message[MAX_MESSAGE_LENGTH];
+            snprintf(message, MAX_MESSAGE_LENGTH, "%s: %s", msg->sender_uid, msg->payload);
+            add_message(message);
+        }
+        else
+        {
+            const char* text = message_code_to_text(code);
+            printf("(0%s) Server: %s\n", msg->payload, text);
+        }
+    }
+}
